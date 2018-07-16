7daf67a12397225aec7fa219d9d523c89a37cdb8
antoniosx10 <32486516+antoniosx10@users.noreply.github.com>
2018-07-16 13:10:23 +0200

    Aggiunto condividi immagine

--- app/src/main/java/unisa/it/pc1/provacirclemenu/ContattiActivity.java
+++ app/src/main/java/unisa/it/pc1/provacirclemenu/ContattiActivity.java
@@ -1,7 +1,10 @@
 package unisa.it.pc1.provacirclemenu;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -18,6 +22,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -72,6 +79,10 @@ public class ContattiActivity extends AppCompatActivity {
         if (i.getType().indexOf("image/") != -1) {
             Log.d("Entrato immagine","si");
             Uri imageUri = (Uri) i.getParcelableExtra(Intent.EXTRA_STREAM);
+
+
+            imagePath = getRealPathFromURI(getApplicationContext(),imageUri);
+
 
             //QUI AGGIUNGERE SALVATAGGIO A FIREBASESTORAGE
 
@@ -147,6 +158,25 @@ public class ContattiActivity extends AppCompatActivity {
         });
 
         return lista;
+    }
+
+
+
+    private String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } catch (Exception e) {
+            return "";
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
     }
 
 }
